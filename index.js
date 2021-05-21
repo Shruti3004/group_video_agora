1778a3fd1e43eb63da6534b65ca6c7a54aaf7ccb
Shruti3004 <mail2shruti.ag@gmail.com>
2021-05-21 14:37:24 +0530

    integrated the RTM with RTC

---
+++ index.js
@@ -0,0 +1,43 @@
+// Params for login
+let options = {
+  uid: "",
+  token: "",
+};
+
+// Your app ID
+const appID = "6714b60932fb4ace8abedfb32cbf2bd0";
+// Your token
+options.token =
+  "0066714b60932fb4ace8abedfb32cbf2bd0IAB4e/ckddN0MXeLiTRLVLEtD/vKCaBQKpxI896BVBOGmqPg45sAAAAAEADjXp+1CXinYAEA6AMJeKdg";
+
+const clientRTM = AgoraRTM.createInstance(appID);
+
+clientRTM.on("MessageFromPeer", function (message, peerId) {
+  console.log("Message from: " + peerId + " Message: " + message);
+});
+// Display connection state changes
+clientRTM.on("ConnectionStateChanged", function (state, reason) {
+  console.log("State changed To: " + state + " Reason: " + reason);
+});
+
+let channel = clientRTM.createChannel("testing");
+
+channel.on("ChannelMessage", function (message, memberId) {
+  console.log("Message received from: " + memberId + " Message: " + message);
+});
+// Display channel member stats
+channel.on("MemberJoined", function (memberId) {
+  console.log(memberId + " joined the channel");
+});
+// Display channel member stats
+channel.on("MemberLeft", function (memberId) {
+  console.log(memberId + " left the channel");
+});
+
+async function joinRTMChannel(uid) {
+  options.uid = uid.toString();
+  console.log(clientRTM);
+  console.log(options);
+  await clientRTM.login(options);
+  console.log("Client LOG IN with this ID");
+}
