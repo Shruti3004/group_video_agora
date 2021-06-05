a6d32a1cdccf746cefbbd70bc991528db7df5685
Shruti3004 <mail2shruti.ag@gmail.com>
2021-06-05 17:22:00 +0530

    updated the token

---
+++ abc.py
@@ -0,0 +1,10 @@
+def multiplyWith4(n):
+ 
+    # returning a number with
+    # multiply with 4 using2
+    # bit shifring right
+    return (n << 2)
+ 
+# derive function
+n = 4
+print(multiplyWith4(n))
